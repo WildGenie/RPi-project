17611c19d7bb103d447c87c5cf2347185f544f7e
aszabo <szaboandras.x@gmail.com>
2014-08-12 04:37:29 +0200

    apro raspberry sharp gpio test modositas

--- raspberry-sharp-io-master/raspberry-sharp-io-master/Tests/Test.Gpio.Chaser/Program.cs
+++ raspberry-sharp-io-master/raspberry-sharp-io-master/Tests/Test.Gpio.Chaser/Program.cs
@@ -13,35 +13,35 @@ namespace Test.Gpio.Chaser
         static void Main(string[] args)
         {
             const ConnectorPin led1Pin = ConnectorPin.P1Pin26;
-            const ConnectorPin led2Pin = ConnectorPin.P1Pin24;
-            const ConnectorPin led3Pin = ConnectorPin.P1Pin22;
-            const ConnectorPin led4Pin = ConnectorPin.P1Pin15;
-            const ConnectorPin led5Pin = ConnectorPin.P1Pin13;
-            const ConnectorPin led6Pin = ConnectorPin.P1Pin11;
-            const ConnectorPin buttonPin = ConnectorPin.P1Pin03;
+            //const ConnectorPin led2Pin = ConnectorPin.P1Pin24;
+            //const ConnectorPin led3Pin = ConnectorPin.P1Pin22;
+            //const ConnectorPin led4Pin = ConnectorPin.P1Pin15;
+            //const ConnectorPin led5Pin = ConnectorPin.P1Pin13;
+            //const ConnectorPin led6Pin = ConnectorPin.P1Pin11;
+            //const ConnectorPin buttonPin = ConnectorPin.P1Pin03;
             
             Console.WriteLine("Chaser Sample: Sample a LED chaser with a switch to change behavior");
             Console.WriteLine();
             Console.WriteLine("\tLed 1: {0}", led1Pin);
-            Console.WriteLine("\tLed 2: {0}", led2Pin);
-            Console.WriteLine("\tLed 3: {0}", led3Pin);
-            Console.WriteLine("\tLed 4: {0}", led4Pin);
-            Console.WriteLine("\tLed 5: {0}", led5Pin);
-            Console.WriteLine("\tLed 6: {0}", led6Pin);
-            Console.WriteLine("\tSwitch: {0}", buttonPin);
+            //Console.WriteLine("\tLed 2: {0}", led2Pin);
+            //Console.WriteLine("\tLed 3: {0}", led3Pin);
+            //Console.WriteLine("\tLed 4: {0}", led4Pin);
+            //Console.WriteLine("\tLed 5: {0}", led5Pin);
+            //Console.WriteLine("\tLed 6: {0}", led6Pin);
+            //Console.WriteLine("\tSwitch: {0}", buttonPin);
             Console.WriteLine();
 
             var driver = args.GetDriver();
 
             // Declare outputs (leds)
-            var leds = new PinConfiguration[]
+            var leds = new PinConfiguration
                            {
                                led1Pin.Output().Name("Led1").Enable(),
-                               led2Pin.Output().Name("Led2"),
-                               led3Pin.Output().Name("Led3").Enable(),
-                               led4Pin.Output().Name("Led4"),
-                               led5Pin.Output().Name("Led5").Enable(),
-                               led6Pin.Output().Name("Led6")
+                               //led2Pin.Output().Name("Led2"),
+                               //led3Pin.Output().Name("Led3").Enable(),
+                               //led4Pin.Output().Name("Led4"),
+                               //led5Pin.Output().Name("Led5").Enable(),
+                               //led6Pin.Output().Name("Led6")
                            };
 
             // Assign a behavior to the leds
@@ -71,16 +71,16 @@ namespace Test.Gpio.Chaser
                                };*/
 
             // Declare input (switchButton) interacting with the leds behavior
-            var switchButton = buttonPin.Input()
-                .Name("Switch")
-                .Revert()
-                .Switch()
-                .Enable()
-                .OnStatusChanged(b =>
-                                     {
-                                         behavior.RoundTrip = !behavior.RoundTrip;
-                                         Console.WriteLine("Button switched {0}", b ? "on" : "off");
-                                     });
+            //var switchButton = buttonPin.Input()
+            //    .Name("Switch")
+            //    .Revert()
+            //    .Switch()
+            //    .Enable()
+            //    .OnStatusChanged(b =>
+            //                         {
+            //                             behavior.RoundTrip = !behavior.RoundTrip;
+            //                             Console.WriteLine("Button switched {0}", b ? "on" : "off");
+            //                         });
 
             // Create connection
             var settings = new GpioConnectionSettings {Driver = driver};
@@ -91,7 +91,7 @@ namespace Test.Gpio.Chaser
 
                 Thread.Sleep(1000);
 
-                connection.Add(switchButton);
+                //connection.Add(switchButton);
                 connection.Start(behavior); // Starting the behavior automatically registers the pins to the connection, if needed.
 
                 Console.ReadKey(true);
